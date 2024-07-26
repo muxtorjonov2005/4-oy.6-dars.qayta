d1409717fdb578b00f958de489dd826bdfa96576
muxtorjonov2005 <muxtorjonoveldiyor@gmail.com>
2024-07-26 22:54:06 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,116 @@
+// let arr = [1, 2, 3, 4, 5, 6]
+// let arr1 = []
+// let max = arr[0]
+// for (const iterator of arr) {
+//     if(max < iterator){
+
+//     }
+// }
+
+
+// 2-masala
+// let arr = ['banana', 5, 'olma', 8, 'uzumzor']
+// let arr1 = []
+// for (const iterator of arr) {
+//     if( typeof iterator == 'string'){
+//         arr1.push(iterator.length)
+//     }
+// }
+// console.log(arr1);
+
+
+// 3-masala
+// let arr = [3, 7, 4, 5, 6]
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] % 2 === 0){
+//         arr[i] = arr[i + 1]
+//     }
+// console.log(arr);}
+
+
+
+// 4-masala
+// let arr = [1, 2, 3, 4, 5, 6]
+// let arr1 = []
+// for(let i = arr.length; i < 0; i++){
+//     if(arr[i] % 2== 1){
+//         arr[i] = 0
+//     }
+// }
+// console.log(arr);
+
+
+// 5-masala
+// let arr = [12, 21, 31, 23]
+// for(let i = 0; i < arr.length; i++){
+//     arr[i] = i
+// }
+// console.log(arr);
+
+
+// 6-masala
+// let arr = [12, 5, 15, 21, 11, 52]
+// let arr1 = []
+// for (const iterator of arr) {
+//     if(iterator % 3 !== 0){
+//         arr1.push(iterator)
+//     }
+// }
+// console.log(arr1);
+
+
+// 7-masala
+// let arr = [12, 5, 41, 31, 51]
+
+
+// 8-masala
+let arr = [3, 7, 4, 5, 6]
+for(let i = 0; i < arr.length; i++){
+    arr[i] = arr[i - 1]
+console.log(arr);}
+
+
+// 9-masala
+// let arr = [-1, 4, -5, 8, -2]
+// let arr1 = []
+// for (const iterator of arr) {
+//     if(iterator > 0){
+//         arr1.push(iterator)
+//     }
+// }
+// console.log(arr1.length);
+
+
+// 10-masala
+// let arr = [-1, 4, -5, 8, -2]
+// let res = 0
+// for (const iterator of arr) {
+//     if(iterator < 0){
+//         res +=iterator 
+//     }
+// }
+// console.log(res);
+
+
+// 11-masala
+// let arr = [1, ]
+
+
+// 12-masala
+// let arr = [1, -2, 4, -5, 6]
+// let res = 1
+// for (const iterator of arr) {
+//     if(iterator > 0){
+//         res *= iterator
+//     }
+// }
+// console.log(res);
+
+
+// 13-masala
+
+
+
+
+
+
